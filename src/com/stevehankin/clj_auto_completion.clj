83d22c5ecfa1c2824a28019d4acba088e238fd22
Steven Hankin <steven.hankin@gmail.com>
2011-07-14 08:05:36 +0100

    Rewrite after some thought

--- src/com/stevehankin/clj_auto_completion.clj
+++ src/com/stevehankin/clj_auto_completion.clj
@@ -1,90 +1,52 @@
-(ns
-    ^{:doc "Auto Completion Tree"
-      :author "Steve Hankin"}
-    com.github.stevenhankin.clj-auto-completion)
 
-(defn
-    ^{:doc "Return an initial empty tree (a node) configured for a maximum selection size ms"
-      :added "1.0.0"}
-   make-tree [ms]
-   (assoc {} :type 'node :max-group-size ms :value-set #{} :child-map {} :radix nil)
-)
 
-(def example-tree (make-tree 2))
 
-(declare add-value-to-node)
-(declare add-value-to-leaf)
+(defn create-tree [] {})
 
-(defn
-    ^{:doc "Return a tree with the supplied value added"
-      :added "1.0.0"}
-   add-value [t v]
-   (let [type (t :type)
-         ms   (t :max-group-size)]
-      (if (= type 'node)
-         (add-value-to-node t v)
-         (add-value-to-leaf t v ms)
+
+; Get matches in tree for string
+(defn get-matches [t [s & more]]
+   (if-let [v (t s)]
+      v
+      (if more
+         (recur t (cons (str s (first more)) (rest more) ) )
+         nil
       )
    )
 )
 
+(def max 2)
 
-(declare create-node)
-
-(defn
-    ^{:doc "Add a value to the node, by retrieving (or creating) the required radix and descending"
-      :private true
-      :added "1.0.0"}
-   add-value-to-node [t [v & more]] ; v represents the current character and determines the radix
-   (let [cm (:child-map t)]
-      (if-let [cb (get cm v)]
-         nil ; TODO - update this block
-         (let [nb (create-node)]  ;  create a new block
-            nb ; TODO - associate in the value
+; add to tree the full string represent by current partial
+(defn add-string
+   ([t f]
+      (add-string t f f)
+   )
+   ([t f part]
+      (let [p (first part)
+            rem (subs part 1)]
+      (if-let [v (t (str p))]
+         (let [size (count v)]
+            (if (< size max)
+               (assoc t (str p) (conj v f))
+               (let  [nt (dissoc t (str p))]
+                  ;(map #(add-string nt % (subs % 0 (inc (count p)))) v )
+                  (add-string nt
+                              (first v) 
+                              (cons (subs (first v) 0 (inc (count part))) 
+                                    (subs (first v) (inc (count part)) ))) 
+               )
+            )
          )
+         (assoc t p (vector f))
       )
-   ) 
-)
-
-(def example-add-value-to-node (add-value-to-node {:child-map {}} "Apple"))
-
-
-(defn
-    ^{:doc "Create an empty node. Similar to make-tree but does not contain the group size"
-      :private true
-      :added "1.0.0"}
-   create-node [] (assoc {} :type 'node :value-set #{} :child-map {} :radix nil)
-)
-
-
-(defn
-    ^{:doc "Add a value to an existing block, potentially converting the block into a node if max size (ms) is reached"
-      :private true
-      :added "1.0.0"}
-   add-value-to-leaf [t [v & more] ms]
-      (let [size (count (t :value-set))]
-         (if (>= size ms)
-            nil ; TODO - make this leaf a block and populate the children
-            (assoc t :value-set (conj (t :value-set) v)) ; add the entry
-         )
-      )
-)
-
-
-(def example-value-add (add-value {:value-set #{\a 1 \b 2 \c 4} :type 'leaf :max-group-size 10} "apple"))
-
-
-(defn
-    ^{:doc "Return 0 or more matching items"
-      :added "1.0.0"}
-   find-partial? [t [v & more]]
-   (let [type (t :type)]
-      (if (= type 'node)
-         nil ; TODO - descend
-         (t :value-set) ; Not a node - so must be a block.  Return the items.
       )
    )
 )
 
+(add-string {"ba" ["bandaid" "banana"] "moo" ["moon" "moonshine" "moomin"]} "banyan")
+
+
+(cons (subs "banyan" 0 (inc (count "ban"))) (subs "banyan" (inc (count "ban")) ))
 
 
